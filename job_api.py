0928eaf0c9a5033fe6306219573e879f539611e8
Vladimir Kondrahin <kondervova2009@gmail.com>
2025-03-15 14:20:53 +0300

    WEB. REST-API. Понятие. Делаем простое Rest-api - Добавление работы

--- job_api.py
+++ job_api.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, redirect, request, render_template
+from flask_login import login_required, current_user
+from forms import LoginForm, GalleryForm, RegisterForm, JobsForm, EditJobsForm, DepartmentForm
+
 from data.db_session import create_session
 from data.users import Jobs
 
@@ -20,3 +23,44 @@ def q_1(id):
         }
         jobs_list.append(job_dict)
     return jsonify(jobs_list)
+
+
+@jobs_api.route('/jobs_delete/<int:id>', methods=['GET', 'POST'])
+def news_delete(id):
+    db_sess = create_session()
+    jobs = db_sess.query(Jobs).filter(Jobs.id == id).first()
+    if jobs:
+        db_sess.delete(jobs)
+        db_sess.commit()
+    else:
+        abort(404)
+    return redirect('/')
+
+
+@jobs_api.route('add/<int:id>', methods=['GET', 'POST'])
+def edit_jobs(id):
+    form = EditJobsForm()
+    if request.method == "GET":
+        db_sess = create_session()
+        jobs = db_sess.query(Jobs).filter(Jobs.id == id).first()
+        if jobs:
+            form.coloborators.data = jobs.collaborators
+            form.work_size.data = jobs.work_size
+            form.job_title.data = jobs.job
+        else:
+            abort(404)
+    if form.validate_on_submit():
+        db_sess = create_session()
+        jobs = db_sess.query(Jobs).filter(Jobs.id == id).first()
+        if jobs:
+            jobs.collaborators = form.coloborators.data
+            jobs.work_size = form.work_size.data
+            jobs.job = form.job_title.data
+            db_sess.commit()
+            return redirect('/')
+        else:
+            abort(404)
+    return render_template('jobs_edit.html',
+                           title='Редактирование новости',
+                           form=form
+                           )
