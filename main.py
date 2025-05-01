d27a5a2f41de2a4442f6f45ae2b053447eeabb23
«Владимир <kondrahinvov@yandex.ru>
2025-05-01 22:04:23 +0300

    Чат-боты 1 (Telegram). Python-telegram-bot - Вежливый эхо-бот

--- main.py
+++ main.py
@@ -1,50 +1,39 @@
-from flask import Flask, request
-import logging
-import json
-from googletrans import Translator
+from telegram.ext import Application
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler
 
-app = Flask(__name__)
 
-logging.basicConfig(level=logging.INFO, filename='app.log', format='%(asctime)s %(levelname)s %(name)s %(message)s')
+user_data = {}
 
-translator = Translator()
 
-@app.route('/post', methods=['POST'])
+async def start(update, context):
+    reply_keyboard = [['/start', '/stop']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
+    user = update.effective_user
+    user_id = update.message.from_user.id
+
+    user_data[user_id] = {}
+
+    await update.message.reply_html(
+        rf"Привет {user.mention_html()}! Я Бот-Тест отправь файл с тестом!",
+        reply_markup=markup
+    )
+
+async def stop(update, context):
+    user_id = update.message.from_user.id
+    if user_id in user_data.keys():
+        del user_data[user_id]
+        await update.message.reply_text('Стоп')
+    else:
+        await update.message.reply_text("Начните сначала с /start.")
+
 def main():
+    application = Application.builder().token('7227875074:AAEoDFAWXsjZCYkHqMx2RnhTMcgJKzI55gU').build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("stop", stop))
+    application.run_polling()
 
-    logging.info('Request: %r', request.json)
-
-    response = {
-        'session': request.json['session'],
-        'version': request.json['version'],
-        'response': {
-            'end_session': False
-        }
-    }
-
-    handle_dialog(response, request.json)
-
-    logging.info('Response: %r', response)
-
-    return json.dumps(response)
-
-
-def handle_dialog(res, req):
-
-    user_id = req['session']['user_id']
-
-    if req['session']['new']:
-        res['response']['text'] = 'Привет! Я умею переводить слова. Скажи: Переведи слово *слово*'
-        return
-        text = req['request']['original_utterance'].lower()
-        if "переведи слово" in text or "переведи" in text :
-            words = text.split()
-            if len(words) > 3:
-                word_to_translate = words[-1]
-                translation = translator.translate(word_to_translate, dest='en', src='ru')
-                res['response']['text'] = translation.text
-            else:
-                res['response']['text'] = "Я не поняла какое слово нужно перевести"
 
 if __name__ == '__main__':
-    app.run()
+    main()
