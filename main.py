edb29d01b96465a723b82d4d7919f48778c3802a
«Владимир <kondrahinvov@yandex.ru>
2025-05-01 22:05:03 +0300

    Чат-боты 1 (Telegram). Python-telegram-bot - Дата и время

--- main.py
+++ main.py
@@ -1,37 +1,48 @@
-from telegram.ext import Application
-from telegram import ReplyKeyboardMarkup
+from telegram.ext import Application, MessageHandler, filters
+
+from datetime import datetime
+
+
+async def echo(update, context):
+    await update.message.reply_text(f'Я получил сообщение- "{update.message.text}"')
+
+
 from telegram.ext import CommandHandler
 
 
-user_data = {}
-
 
 async def start(update, context):
-    reply_keyboard = [['/start', '/stop']]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
     user = update.effective_user
-    user_id = update.message.from_user.id
-
-    user_data[user_id] = {}
-
     await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Я Бот-Тест отправь файл с тестом!",
-        reply_markup=markup
+        rf"Привет {user.mention_html()}! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!",
     )
 
-async def stop(update, context):
-    user_id = update.message.from_user.id
-    if user_id in user_data.keys():
-        del user_data[user_id]
-        await update.message.reply_text('Стоп')
-    else:
-        await update.message.reply_text("Начните сначала с /start.")
+
+async def help_command(update, context):
+    await update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
+
+
+async def date_command(update, context):
+    dt = datetime.now()
+    res = dt.strftime('%Y-%m-%d')
+    await update.message.reply_text(f"Сегодня {res}")
+
+
+async def time_command(update, context):
+    dt = datetime.now().time()
+    res = dt.strftime('%H часов %M минут %S секунд')
+    await update.message.reply_text(f"Сейчас {res}")
+
 
 def main():
     application = Application.builder().token('7227875074:AAEoDFAWXsjZCYkHqMx2RnhTMcgJKzI55gU').build()
+    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("stop", stop))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("date", date_command))
+    application.add_handler(CommandHandler("time", time_command))
+    application.add_handler(text_handler)
     application.run_polling()
 
 
