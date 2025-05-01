9a2e69bcdc9f0652227e8a0b43a7792f0572bb57
«Владимир <kondrahinvov@yandex.ru>
2025-05-01 22:07:10 +0300

    Чат-боты 1 (Telegram). Python-telegram-bot  - Бот-экскурсовод или «в греческом зале...»

--- main.py
+++ main.py
@@ -1,53 +1,48 @@
 from telegram.ext import Application, MessageHandler, filters
 from telegram import ReplyKeyboardMarkup
 from telegram import ReplyKeyboardRemove
-from telegram.ext import CommandHandler, ConversationHandler
+from telegram.ext import CommandHandler
 from random import randint
 
 
 async def start(update, context):
+    reply_keyboard = [['Вход']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Привет {user.mention_html()}! Я Бот-Экскурсовот!",
+        reply_markup=markup
+    )
+
+
+async def exs(update, context):
+    zals = {'Зал 1': ([['Зал 2', 'Выход']], 'декорации первого зала'),
+            'Зал 2': ([['Зал 3']], 'что-то из второго зала'),
+            'Зал 3': ([['Зал 1', 'Зал 4']], 'штуки из третьего зала'),
+            'Зал 4': ([['Зал 1']], 'мега интересное из четвёртого зала'),
+    }
+    text = update.message.text
+    if text == 'Вход':
+        text = 'Зал 1'
+
+    markup = ReplyKeyboardMarkup(zals[text][0], one_time_keyboard=False)
     await update.message.reply_text(
-        "Привет. Пройдите небольшой опрос, пожалуйста!\n"
-        "В каком городе вы живёте?", reply_markup=ReplyKeyboardRemove())
-    return 1
-
-
-async def first_response(update, context):
-    locality = update.message.text
-    print(locality)
-    if locality != '/skip':
-        await update.message.reply_text(
-            f"Какая погода в городе {locality}?")
-    else:
-        await update.message.reply_text(
-            f"Какая погода у вас за окном?")
-    return 2
-
-
-async def second_response(update, context):
-    weather = update.message.text
-    await update.message.reply_text("Спасибо за участие в опросе! Всего доброго!")
-    return ConversationHandler.END
-
-
-async def stop(update, context):
-    await update.message.reply_text("Всего доброго!")
-    return ConversationHandler.END
+        "Добро пожаловать! Пожалуйста, сдайте верхнюю одежду в гардероб! "
+        f"В данном зале представлено {zals[text][1]}."
+        " Всего доброго, не забудьте забрать верхнюю одежду в гардеробе!",
+        reply_markup=markup
+    )
 
 
 
 def main():
     application = Application.builder().token('7227875074:AAEoDFAWXsjZCYkHqMx2RnhTMcgJKzI55gU').build()
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-        states={
-            1: [MessageHandler(filters.TEXT, first_response)],
-            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response)]
-        },
-        fallbacks=[CommandHandler('stop', stop)]
-    )
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.Regex("^(Выход)$"), start))
 
-    application.add_handler(conv_handler)
+    application.add_handler(MessageHandler(filters.Regex(
+        '^(Зал 1|Зал 2|Зал 3|Зал 4|Вход)$'), exs))
     application.run_polling()
 
 
