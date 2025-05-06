ec61223067ec36680ff998ebe1141b49f6101078
«Владимир <kondrahinvov@yandex.ru>
2025-05-06 10:20:03 +0300

    Чат-боты 2 (ВКонтакте) - Запрос Записи на стене

--- main.py
+++ main.py
@@ -1,75 +1,56 @@
-import aiohttp
-from telegram.ext import Application, MessageHandler, filters
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import CommandHandler
+import vk_api
+import datetime
 
 
-user_data = {}
+def captcha_handler(captcha):
+    """ При возникновении капчи вызывается эта функция и ей передается объект
+        капчи. Через метод get_url можно получить ссылку на изображение.
+        Через метод try_again можно попытаться отправить запрос с кодом капчи
+    """
 
-async def geocoder(update, context):
-    user_id = update.message.from_user.id
-    if user_id in user_data.keys():
-        try:
-            geocoder_uri = "http://geocode-maps.yandex.ru/1.x/"
-            response = await get_response(geocoder_uri, params={
-                "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
-                "format": "json",
-                "geocode": update.message.text
-            })
+    key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
 
-            toponym = response["response"]["GeoObjectCollection"][
-                "featureMember"][0]["GeoObject"]
-            ll = toponym["Point"]["pos"].split()
-            static_api_request = f"http://static-maps.yandex.ru/1.x/?ll={ll[0]},{ll[1]}&spn=0.18,0.18&pt={ll[0]},{ll[1]}&l=map"
-            await context.bot.send_photo(
-                update.message.chat_id,
-                static_api_request,
-                caption=f"Вот что нашёл по запросу: {update.message.text}"
-            )
-        except:
-            await update.message.reply_text("Ошибка: Введите корректно запрос")
-    else:
-        await update.message.reply_text("Начните сначала с /start.")
+    # Пробуем снова отправить запрос с капчей
+    return captcha.try_again(key)
 
 
-async def get_response(url, params):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params) as resp:
-            return await resp.json()
+def auth_handler():
+    """ При двухфакторной аутентификации вызывается эта функция. """
 
+    # Код двухфакторной аутентификации,
+    # который присылается по смс или уведомлением в мобильное приложение
+    # или код из приложения - генератора кодов
+    key = input("Enter authentication code: ").strip()
+    # Если: True - сохранить, False - не сохранять.
+    remember_device = True
 
-async def start(update, context):
-    reply_keyboard = [['/start', '/stop']]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
-
-    user = update.effective_user
-    user_id = update.message.from_user.id
-
-    user_data[user_id] = {
-        'right': 0,
-    }
-
-    await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Я Бот-Геокодер отправь мне название города, а я выведу карту этого объекта!",
-        reply_markup=markup
-    )
-
-async def stop(update, context):
-    user_id = update.message.from_user.id
-    if user_id in user_data.keys():
-        del user_data[user_id]
-        await update.message.reply_text('Стоп игра')
-    else:
-        await update.message.reply_text("Начните сначала с /start.")
+    return key, remember_device
 
 
 def main():
-    application = Application.builder().token('7227875074:AAEoDFAWXsjZCYkHqMx2RnhTMcgJKzI55gU').build()
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("stop", stop))
-    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, geocoder)
-    application.add_handler(text_handler)
-    application.run_polling()
+    LOGIN, PASSWORD = 'kondrahinvov@yandex.ru', 'Konder12!'
+    login, password = LOGIN, PASSWORD
+    vk_session = vk_api.VkApi(
+        login, password,
+        # функция для обработки двухфакторной аутентификации
+        auth_handler=auth_handler,
+        captcha_handler=captcha_handler
+    )
+
+    try:
+        vk_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+    vk = vk_session.get_api()
+    # Используем метод wall.get
+    response = vk.wall.get(count=5, offset=0)
+    if response['items']:
+        for num, i in response['items']:
+            date, time = datetime.datetime.fromtimestamp(i['date']).strftime('%Y-%m-%d %H:%M:%S').split()
+            print('Номер', num)
+            print(i['text'])
+            print(f'date: {date}, time: {time}')
 
 
 if __name__ == '__main__':
