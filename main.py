400d4978928671dd27419e541a01e04634bfb5f9
«Владимир <kondrahinvov@yandex.ru>
2025-05-06 19:03:54 +0300

    Чат-боты 2 (ВКонтакте) - Бот Википедия 2.0

--- main.py
+++ main.py
@@ -1,79 +1,58 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
-import requests
-from io import BytesIO
+import wikipedia
+from warnings import filterwarnings
 
-
-
-def map(main_coords, add_params=None):
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll={main_coords[0]},{main_coords[1]}&spn=0.18,0.18&l=map"
-    if add_params:
-        map_request += "&" + add_params
-    response = requests.get(map_request)
-    return BytesIO(response.content)
-
-
-def get_coord(adress):
-    server_address = "http://geocode-maps.yandex.ru/1.x/?"
-    api_key = "8013b162-6b42-4997-9691-77b7074026e0"
-    geocode = adress
-    geocoder_request = f"{server_address}apikey={api_key}&geocode={geocode}&format=json"
-    response = requests.get(geocoder_request)
-    if response:
-        json_response = response.json()
-        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0][
-            "GeoObject"
-        ]
-        toponym_coodrinates = toponym["Point"]["pos"]
-        return float(toponym_coodrinates.split()[0]), float(
-            toponym_coodrinates.split()[1]
-        )
-    else:
-        print("Ошибка выполнения запроса:")
-        print(geocoder_request)
-        print("Http статус:", response.status_code, "(", response.reason, ")")
+# Игнорируем предупреждение BeautifulSoup
+filterwarnings("ignore", category=UserWarning, module="wikipedia")
 
 
 def main():
     vk_session = vk_api.VkApi(
-        token="vk1.a.iWGJ7nURr_L_JftonlcwC-uZzusx6Kz56uVl_ba9W1rpT8kH_AwXEIOxuYKpC1MbZJdaieC2sx-4OANCe_TqEjWXJDKUz-qRha6PFhm49WQ0NQvz0VoMJU1RbPtdOMpUC4ArZqM1EdqSyFnojyQxAmvFQoUGLcwZN6lAwDHn3T6-uM_xx01AhuAa2FFyloLFD2ROX82bk-anrG0gfYz5rw"
-    )
+        token='vk1.a.iWGJ7nURr_L_JftonlcwC-uZzusx6Kz56uVl_ba9W1rpT8kH_AwXEIOxuYKpC1MbZJdaieC2sx-4OANCe_TqEjWXJDKUz-qRha6PFhm49WQ0NQvz0VoMJU1RbPtdOMpUC4ArZqM1EdqSyFnojyQxAmvFQoUGLcwZN6lAwDHn3T6-uM_xx01AhuAa2FFyloLFD2ROX82bk-anrG0gfYz5rw')
 
-    longpoll = VkBotLongPoll(vk_session, "230383848")
+    longpoll = VkBotLongPoll(vk_session, 230383848)  # ID без кавычек
+    vk = vk_session.get_api()
+
+    print("Бот запущен и ожидает сообщений...")
+
     for event in longpoll.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
-            print("Новое сообщение:")
-            print("Для меня от:", event.obj.message["from_id"])
-            print("Текст:", event.obj.message["text"])
+            msg = event.obj.message
+            user_id = msg['from_id']
+            text = msg['text']
 
-            vk = vk_session.get_api()
+            print(f'Новое сообщение от {user_id}: {text}')
 
-            ans_user = vk.users.get(user_id=event.obj.message["from_id"])[0]
             try:
-                ans_name, ans_surname = ans_user["first_name"], ans_user["last_name"]
-            except Exception as e:
-                ans_name, ans_surname = "пользователь", ""
-            try:
-                main_coords = get_coord(event.obj.message["text"])
-                map_image = map(main_coords)
-                upload = vk_api.VkUpload(vk_session)
-                photo = upload.photo_messages(map_image)[0]
-                attachment = f"photo{photo['owner_id']}_{photo['id']}"
-                vk.messages.send(
-                    user_id=event.obj.message["from_id"],
-                    message=f"{ans_name} {ans_surname} вот что я нашёл",
-                    random_id=random.getrandbits(31),
-                    attachment=attachment,
-                )
-            except Exception as e:
-                vk.messages.send(
-                    user_id=event.obj.message["from_id"],
-                    message=f"{ans_name} {ans_surname} введи корректное название",
-                    random_id=random.getrandbits(31),
-                )
-                print(e)
+                user_info = vk.users.get(user_ids=user_id)[0]
+                full_name = f"{user_info['first_name']} {user_info['last_name']}"
+            except Exception:
+                full_name = "пользователь"
+            if text.strip().endswith('?'):
+                query = text.strip()[:-1]
+                try:
+                    page = wikipedia.page(query, auto_suggest=False)
+                    summary = page.summary[:1000]
+                    response = f"{summary}\n\nПодробнее: {page.url}"
+                except wikipedia.exceptions.DisambiguationError as e:
+                    options = "\n• ".join(e.options[:5])
+                    response = f"Уточните запрос. Возможные варианты:\n• {options}"
+                except wikipedia.exceptions.PageError:
+                    response = "Не удалось найти информацию по вашему запросу."
+                except Exception as e:
+                    response = f"Произошла ошибка: {str(e)}"
+            else:
+                response = f"{full_name}, поставьте знак вопроса в конце сообщения, чтобы я мог ответить."
+
+            vk.messages.send(
+                user_id=user_id,
+                message=response,
+                random_id=random.getrandbits(31)
+            )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    wikipedia.set_lang("ru")
     main()
