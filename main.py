939778c09f11c02a9a346c0e697c55567c455eed
«Владимир <kondrahinvov@yandex.ru>
2025-05-06 15:34:52 +0300

    Чат-боты 2 (ВКонтакте) - Получение фото из альбома

--- main.py
+++ main.py
@@ -1,41 +1,69 @@
 import vk_api
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-import random
-import wikipedia
+import os
+
+
+def captcha_handler(captcha):
+    """ При возникновении капчи вызывается эта функция и ей передается объект
+        капчи. Через метод get_url можно получить ссылку на изображение.
+        Через метод try_again можно попытаться отправить запрос с кодом капчи
+    """
+
+    key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
+
+    # Пробуем снова отправить запрос с капчей
+    return captcha.try_again(key)
+
+
+def auth_handler():
+    """ При двухфакторной аутентификации вызывается эта функция. """
+
+    # Код двухфакторной аутентификации,
+    # который присылается по смс или уведомлением в мобильное приложение
+    # или код из приложения - генератора кодов
+    key = input("Enter authentication code: ").strip()
+    # Если: True - сохранить, False - не сохранять.
+    remember_device = True
+
+    return key, remember_device
+
+
+def get_photos_from_album(vk_session, group_id, album_id):
+    vk = vk_session.get_api()
+    photos = vk.photos.get(
+        owner_id=-group_id,
+        album_id=album_id,
+     )
+    print(f"\nНайдено {photos['count']} фотографий в альбоме:")
+    for i, photo in enumerate(photos['items'], 1):
+        print(f"\nФото #{i}:")
+        print(f"ID: {photo['id']}")
+        print(f"Дата: {photo['date']}")
+        or_ph = photo['orig_photo']
+        print(f'{or_ph['height']}x{or_ph['width']}')
+        print(f'URL: {or_ph['url']}')
+
+    return photos['items']
 
 
 def main():
+    LOGIN, PASSWORD = 'kondrahinvov@yandex.ru', 'Konder12!'
+    login, password = LOGIN, PASSWORD
     vk_session = vk_api.VkApi(
-        token='vk1.a.iWGJ7nURr_L_JftonlcwC-uZzusx6Kz56uVl_ba9W1rpT8kH_AwXEIOxuYKpC1MbZJdaieC2sx-4OANCe_TqEjWXJDKUz-qRha6PFhm49WQ0NQvz0VoMJU1RbPtdOMpUC4ArZqM1EdqSyFnojyQxAmvFQoUGLcwZN6lAwDHn3T6-uM_xx01AhuAa2FFyloLFD2ROX82bk-anrG0gfYz5rw')
+        login, password,
+        auth_handler=auth_handler,
+        captcha_handler=captcha_handler
+    )
 
-    longpoll = VkBotLongPoll(vk_session, '230383848')
-    for event in longpoll.listen():
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            print('Для меня от:', event.obj.message['from_id'])
-            print('Текст:', event.obj.message['text'])
+    try:
+        vk_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
 
-            vk = vk_session.get_api()
+    GROUP_ID = 230383848
+    ALBUM_ID = 307208634
+    photos = get_photos_from_album(vk_session, GROUP_ID, ALBUM_ID)
 
-            ans_user = vk.users.get(user_id=event.obj.message['from_id'])[0]
-            try:
-                ans_name, ans_surname = ans_user['first_name'], ans_user['last_name']
-            except Exception as e:
-                ans_name, ans_surname = 'пользователь', ''
-
-
-            if '?' == event.obj.message['text'][-1]:
-                page = wikipedia.page(event.obj.message['text'])
-                summary = page.summary
-                ans = summary[:1000]
-                vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message=ans,
-                                 random_id=random.getrandbits(31))
-            else:
-                vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message=f"{ans_name} {ans_surname} поставьте вопрос в конце сообщение чтобы я мог на него ответить",
-                                 random_id=random.getrandbits(31))
 
 if __name__ == '__main__':
-    wikipedia.set_lang("ru")
     main()
